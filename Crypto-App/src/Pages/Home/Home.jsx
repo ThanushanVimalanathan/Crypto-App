122a21b52623facc1a8d41d39b11492089b962df
VimalanathanThanushan <thanushaan69@gmail.com>
2026-01-05 20:01:08 +0530

    Feat:Create routing

---
+++ Crypto-App/src/Pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='hero'>
+        <h1>Largest <br />Crypto Marketplace</h1>
+        <p>Your gateway to the decentralized future. A Web3 wallet is more than a 
+          place to store digital currency; it is your secure identity and passport 
+          to blockchain applications, allowing you to manage assets, interact with 
+          smart contracts, and explore the new frontier of the internet with complete
+           control and ownership.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
