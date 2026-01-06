b5c4f6ac0903ddb1ad2cdfe23b8a6f22139bb9fe
VimalanathanThanushan <thanushaan69@gmail.com>
2026-01-06 20:24:14 +0530

    FEAT:Create search bar fuctionality

--- Crypto-App/src/Pages/Home/Home.jsx
+++ Crypto-App/src/Pages/Home/Home.jsx
@@ -6,6 +6,23 @@ const Home = () => {
 
   const {allCoin,currency} = useContext(CoinContext)
   const [displayCoin,setDisplayCoin] = useState([]);
+  const [input,setInput] = useState('');
+
+  const inputHandler =(event)=>{
+      setInput(event.target.value);
+      if(event.target.value === ""){
+        setDisplayCoin(allCoin)
+      }
+  }
+
+  const searchHandler = async (event) =>{
+      event.preventDefault();
+     const coins =  await allCoin.filter((item)=>{
+         return item.name.toLowerCase().includes(input.toLowerCase())
+          
+      })
+      setDisplayCoin(coins);
+  }
   
 
   useEffect(()=>{
@@ -23,8 +40,16 @@ const Home = () => {
           smart contracts, and explore the new frontier of the internet with complete
            control and ownership.
         </p>
-        <form >
-          <input type="text" placeholder='Search crypto...' />
+        <form onSubmit={searchHandler} >
+
+         
+
+          <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto...' required />
+
+           <datalist id='coinlist'>
+            {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+           </datalist>
+
           <button type='submit'>Search</button>
         </form>
 
